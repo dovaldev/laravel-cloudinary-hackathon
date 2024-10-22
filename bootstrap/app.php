0488e96f5664fef45c23b8d2b118e8d01088c8f8
J.A. Doval <73448899+dovaldev@users.noreply.github.com>
2024-10-22 13:26:08 +0200

    Actualizar archivo de configuración app.php: Agregar excepción para la validación de tokens CSRF en la URL de redirección de Google

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'https://profileai.top/auth/google/callback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
